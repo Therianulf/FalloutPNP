f857efc5713e43ad93f8ef978b0b0dfdfc478934
therian <therianlarson@gmail.com>
2017-03-26 15:26:49 -0700

    chris fix woot thanks

--- resources/assets/js/new_character/attribute.js
+++ resources/assets/js/new_character/attribute.js
@@ -2,6 +2,7 @@
  * Created by therian on 3/26/2017.
  */
 
+/*
 $( document ).ready(
     function() {
         $(":input[type='number']").bind('keyup mouseup', function () {
@@ -12,10 +13,30 @@ $( document ).ready(
             )
         });
 });
+*/
 
 
 
+var globalParamTotal = 0; //this has to exist outside of the function
+$( document ).ready(
+    function(){
+        $(":input[type='number']").bind('keyup mouseup', function (event) {
+            checkTotal();
+            console.log(globalParamTotal);
+        });
+    }
+);
 
+function checkTotal(){
+    var numberInputs = $(document).find(":input[type='number']"),
+        localParamTotal = 0;
+    $.each(numberInputs, function(index, obj){
+        var currentVal = parseInt($(obj).val(),10);
+        localParamTotal = localParamTotal + currentVal;
+    });
+    console.log(localParamTotal);
+    globalParamTotal = localParamTotal;
+}
 
 
 function check_attribute() {
--- resources/views/new_character/name.blade.php
+++ resources/views/new_character/name.blade.php
@@ -6,13 +6,14 @@
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
                     <div class="panel-heading">New Character</div>
-
                     <div class="panel-body">
-
-
                         <label for="first_name">Character's First Name</label> <input type="text" name="first_name" id="first_name">
                         <br/>
                         <label for="first_name">Character's Last Name</label> <input type="text" name="last_name" id="last_name">
+                        <br/>
+                        <label for="character_total_points">Character's Total Points</label> <strong  id="character_total_points" name="character_total_points">40</strong>
+                        <br/>
+                        <label for="character_total_left">Character's Total Points</label> <strong  id="character_total_left" name="character_total_left">5</strong>
                         <br/>
                         <label for="character_strength">Character's Strength</label> <input type="number" value="5" id="character_strength" name="character_strength">
                         <br/>
@@ -26,11 +27,7 @@
                         <br/>
                         <label for="character_agility">Character's Agility</label> <input type="number" value="5" id="character_agility" name="character_agility">
                         <br/>
-                        <label for="character_agility">Character's Agility</label> <input type="number" value="5" id="character_agility" name="character_agility">
-
-
-
-
+                        <label for="character_luck">Character's Luck</label> <input type="number" value="5" id="character_luck" name="character_luck">
                     </div>
                 </div>
             </div>
