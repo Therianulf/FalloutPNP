92859b111f74e3c7ddaedf65fa77ce2f44fd10a5
Chris Sandoval <chrissandoval916@gmail.com>
2017-10-03 23:47:54 -0700

    Init commit for pipboy CSS. Form currently broken right now...
    - Figured out issue with jquery min css...needed to be a scss file for import
    - Added scss file for attributes page (some of this css should be moved to global lvl eventually)
    - Template layout changed for pipboy look/feel

--- resources/views/new_character/attributes.blade.php
+++ resources/views/new_character/attributes.blade.php
@@ -1,62 +1,109 @@
 @extends('layouts.new_char_layout')
 
 @section('content')
-    <div class="attributesPage container" data-role="attributesPage">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">New Character</div>
-                    <div class="panel-body">
-                        <div id="statProgressBar"></div>
-                        <form method="post" action="/attributes_handler" >
-                            {{ csrf_field() }}
-                            <div id="sectionCont">
-                                <h3>Character Details</h3>
-                                <div>
-                                    <input type="text" name="attributes[first_name]" placeholder="First Name"/>
-                                    <input type="text" name="attributes[last_name]" placeholder="Last Name"/>
-                                    <br />
-                                    <label for="charDesc">Character's Description:</label>
-                                    <br />
+    <div class="attributesPage" data-role="attributesPage">
+        <div id="overlay"></div>
+        <div class="pageContent">
+            <div class="header">
+                <div class="pgTitle">
+                    <span class="sep left"></span>
+                    <span class="title">Bio</span>
+                    <span class="sep right"></span>
+                </div>
+                <div class="hpCont statCont">
+                    <span class="title">HP</span>
+                    <span class="value">???/???</span>
+                </div>
+                <div class="apCont statCont">
+                    <span class="title">AP</span>
+                    <span class="value">0/0</span>
+                </div>
+                <div class="xpCont statCont">
+                    <span class="title">XP</span>
+                    <span class="value">0/???</span>
+                </div>
+            </div>
+            <div class="contentSection cf">
+                <div class="mainContent">
+                    <form method="post" action="/attributes_handler" >
+                        {{ csrf_field() }}
+                        <div class="contentCont" data-tile="Bio">
+                            <ul>
+                                <li>
+                                    <label for="firstName">First Name:</label>
+                                    <input id="firstName" type="text" name="attributes[first_name]" />
+                                </li>
+                                <li>
+                                    <label for="lastName">Last Name:</label>
+                                    <input id="lastName" type="text" name="attributes[last_name]" />
+                                </li>
+                                <li>
+                                    <label for="charDesc">Character Bio:</label>
                                     <textarea id="charDesc" name="attributes[character_description]" rows="4" cols="50"></textarea>
-                                </div>
-                                <h3>S.P.E.C.I.A.L. Attributes</h3>
-                                <div>
-                                    <div class="optionCont">
-                                        <label for="strength">Character's Strength:</label>
-                                        <input id="strength" name="attributes[strength]" data-role="spinner">
-                                    </div>
-                                    <div class="optionCont">
-                                        <label for="perception">Character's Perception:</label>
-                                        <input id="perception" name="attributes[perception]" data-role="spinner">
-                                    </div>
-                                    <div class="optionCont">
-                                        <label for="endurance">Character's Endurance:</label>
-                                        <input id="endurance" name="attributes[endurance]" data-role="spinner">
-                                    </div>
-                                    <div class="optionCont">
-                                        <label for="charisma">Character's Charisma:</label>
-                                        <input id="charisma" name="attributes[charisma]" data-role="spinner">
-                                    </div>
-                                    <div class="optionCont">
-                                        <label for="intelligence">Character's Intelligence:</label>
-                                        <input id="intelligence" name="attributes[intelligence]" data-role="spinner">
-                                    </div>
-                                    <div class="optionCont">
-                                        <label for="agility">Character's Agility:</label>
-                                        <input id="agility" name="attributes[agility]" data-role="spinner">
-                                    </div>
-                                    <div class="optionCont">
-                                        <label for="luck">Character's Luck:</label>
-                                        <input id="luck" name="attributes[luck]" data-role="spinner">
-                                    </div>
-                                </div>
-                            </div>
-                            <div>
-                                <button class="formSubmit" type="submit" >Submit</button>
-                            </div>
-                        </form>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="contentCont hide" data-title="S.P.E.C.I.A.L. Attributes">
+                            <ul>
+                                <li class="spinnerCont">
+                                    <label for="strength">Strength:</label>
+                                    <input id="strength" name="attributes[strength]" data-role="spinner">
+                                </li>
+                                <li class="spinnerCont">
+                                    <label for="perception">Perception:</label>
+                                    <input id="perception" name="attributes[perception]" data-role="spinner">
+                                </li>
+                                <li class="spinnerCont">
+                                    <label for="endurance">Endurance:</label>
+                                    <input id="endurance" name="attributes[endurance]" data-role="spinner">
+                                </li>
+                                <li class="spinnerCont">
+                                    <label for="charisma">Charisma:</label>
+                                    <input id="charisma" name="attributes[charisma]" data-role="spinner">
+                                </li>
+                                <li class="spinnerCont">
+                                    <label for="intelligence">Intelligence:</label>
+                                    <input id="intelligence" name="attributes[intelligence]" data-role="spinner">
+                                </li>
+                                <li class="spinnerCont">
+                                    <label for="agility">Agility:</label>
+                                    <input id="agility" name="attributes[agility]" data-role="spinner">
+                                </li>
+                                <li class="spinnerCont">
+                                    <label for="luck">Luck:</label>
+                                    <input id="luck" name="attributes[luck]" data-role="spinner">
+                                </li>
+                            </ul>
+                        </div>
+                    </form>
+                </div>
+                <div class="subContent">
+                    <div class="bioCont contentCont">
+                        <div class="titleCont">
+                            <span class="sep left"></span>
+                            <span class="title">Who are you?</span>
+                            <span class="sep right"></span>
+                        </div>
+                        <p>Will your character be a gun-happy sniper? A dune-buggy racer?
+                            A sneaky thief? A fast talker who can squeak past the armed guards with a good excuse?
+                            A boxing champ, strong but slow? A beautiful seductress who takes what she wants after the moment?
+                            The possibilities are endless.</p>
                     </div>
+                    <div class="specialCont contentCont hide">
+                        <div id="statProgressBar"></div>
+                        <ul>
+                            <li></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="footer">
+                <div class="subMenu">
+                    <div class="sep left"></div>
+                    <a class="active" href="#">Bio</a>
+                    <a href="#">S.P.E.C.I.A.L.</a>
+                    <a href="#">Done</a>
+                    <div class="sep right"></div>
                 </div>
             </div>
         </div>
