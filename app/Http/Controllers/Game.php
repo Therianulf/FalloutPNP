805129d2a575a3bf650958444dc64d92155fe29b
therianulf <therianlarson@gmail.com>
2017-10-11 13:23:48 -0700

    routing character selector

--- app/Http/Controllers/Game.php
+++ app/Http/Controllers/Game.php
@@ -2,14 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\character;
+use App\character_skills;
+use App\character_stats;
 use Illuminate\Http\Request;
 
 class Game extends Controller
 {
     public function game(Request $request){
-        return view('fallout');
+        $characters = character::where('user_id',$request->user()->id)->get();
+        return view('fallout',['characters'=>$characters]);
     }
 
+    public function character(Request $request){
+        return view('character.selected_char',
+            [
+                'character'=>character::find($request->input('character_id')),
+                'stats'=>character_stats::find($request->input('character_id')),
+                'skills'=>character_skills::find($request->input('character_id'))
+            ]);
+    }
 
 
 }
--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,14 @@
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware('guest');
 
 Route::get('/home',"Game@game");
-Route::get('/new_char_name',"character_creator@new_character");
-Route::post('/attributes_handler',"character_creator@attributes_handler");
-Route::get('/new_char_skills',"character_creator@character_skills");
-Route::post('/skills_handler',"character_creator@character_skills_handler");
+Route::get('/new_char_name',"character_creator@new_character")->middleware('auth');
+Route::post('/attributes_handler',"character_creator@attributes_handler")->middleware('auth');
+Route::get('/new_char_skills',"character_creator@character_skills")->middleware('auth');
+Route::post('/skills_handler',"character_creator@character_skills_handler")->middleware('auth');
+Route::post('/selected_character','game@character')->middleware('auth');
 Route::get('/test1',"test@test1");
 Route::get('/gm_screen','game_master@game_master_screen');
 Route::get('/gm_monsters','game_master@game_master_monsters');
