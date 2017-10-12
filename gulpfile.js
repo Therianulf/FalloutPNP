4bd001ac0f1af096fe10808b407cd6a3ee0b47da
Chris Sandoval <chrissandoval916@gmail.com>
2017-10-12 00:37:33 -0700

    Selected char flow
    - Made selected char blade look + function like skills page
    - Alphabetized Skills and Selected Char page

--- gulpfile.js
+++ gulpfile.js
@@ -18,4 +18,5 @@ elixir(function(mix){
        .webpack('app.js');
     mix.scripts(['jquery-3.2.0.min.js','jquery-ui.min.js','new_character/attribute.js', 'new_character/skills.js'],"public/js/new_char.js");
     mix.scripts(['jquery-3.2.0.min.js','jquery-ui.min.js','test/test.js'],"public/js/test1.js");
+    mix.scripts(['jquery-3.2.0.min.js','jquery-ui.min.js','new_character/selected_char.js'],"public/js/charWidget.js");
 });
